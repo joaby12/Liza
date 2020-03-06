dffb7f15ed9008f5cae1f2220869adbedae77d2e
joaby <joabyaugusto@hotmail.com>
2020-03-06 16:56:16 -0300

    commit

--- colibri/settings.py
+++ colibri/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-
+AUTH_USER_MODEL = 'base.User'
 
 # Application definition
 
