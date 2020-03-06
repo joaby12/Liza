541b55c789d69814dbe76a890c6e0cf4632b8fad
joaby <joabyaugusto@hotmail.com>
2020-03-06 10:00:59 -0300

    commit

--- colibri/settings.py
+++ colibri/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from decouple import config
-from decouple import Csv, config
+from decouple import Csv
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
